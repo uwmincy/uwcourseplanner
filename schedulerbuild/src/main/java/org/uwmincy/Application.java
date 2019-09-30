5cb82b2158be7bf1e56ec1179d205b6921e8bf32
tomzyzhu <tzzhu@edu.uwaterloo.ca>
2019-09-30 18:21:35 -0400

    change server to real test server

--- schedulerbuild/src/main/java/org/uwmincy/Application.java
+++ schedulerbuild/src/main/java/org/uwmincy/Application.java
@@ -3,9 +3,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationMain {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationMain.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
